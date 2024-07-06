9680e48c8e585bd88a36e1234ee5c622d70203da
WizMager <wizmager@gmail.com>
2024-07-06 11:18:37 +0300

    -fix problem with generate cache from addressables

--- Assets/Scripts/Generator/InstallerGenerator.cs
+++ Assets/Scripts/Generator/InstallerGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -83,3 +84,4 @@ namespace Generator
         }
     }
 }
+#endif
