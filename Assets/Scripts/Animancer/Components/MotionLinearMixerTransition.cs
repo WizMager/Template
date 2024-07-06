506792f86277bc67c9d90899f8ab05072f012aee
Ivanus <vivatik.pumpkin.eater@gmail.com>
2024-07-06 12:23:04 +0300

    add motion transition

---
+++ Assets/Scripts/Animancer/Components/MotionLinearMixerTransition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Animancer.Components
+{
+    [Serializable]
+    public class MotionLinearMixerTransition : LinearMixerTransition
+    {
+        [SerializeField] private bool applyRootMotion;
+
+        /// <inheritdoc />
+        public override void Apply(AnimancerState state)
+        {
+            base.Apply(state);
+            state.Root.Component.Animator.applyRootMotion = applyRootMotion;
+        }
+    }
+}
