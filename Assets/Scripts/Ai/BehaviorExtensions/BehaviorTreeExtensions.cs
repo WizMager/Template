ffd2a9cec2707d11679e87a85fd853f709c6b677
Ivanus <vivatik.pumpkin.eater@gmail.com>
2024-07-07 11:14:23 +0300

    best friend behaviorTree + Zenject

---
+++ Assets/Scripts/Ai/BehaviorExtensions/BehaviorTreeExtensions.cs
@@ -0,0 +1,34 @@
+﻿using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using Zenject;
+
+namespace Ai.BehaviorExtensions
+{
+    public static class BehaviorTreeExtensions
+    {
+        public static void QueueAllTasksForInject(this BehaviorTree tree, DiContainer container)
+        {
+            tree.CheckForSerialization(true, true);
+
+            tree.OnBehaviorStart += behavior => { InjectIntoBehavior(container, behavior); };
+        }
+
+        private static void InjectIntoBehavior(DiContainer container, Behavior behavior)
+        {
+            foreach (var task in behavior.FindTasks<Task>())
+            {
+                if (task is BehaviorTreeReference referenceTask)
+                {
+                    foreach (var externalBehavior in referenceTask.GetExternalBehaviors())
+                    {
+                        externalBehavior.Init();
+                        var tasks = externalBehavior.FindTasks<Task>();
+                        tasks.ForEach(container.Inject);
+                    }
+                }
+
+                container.Inject(task);
+            }
+        }
+    }
+}
